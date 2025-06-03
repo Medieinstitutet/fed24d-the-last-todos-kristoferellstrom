5202dcfad5b4475b559a034be54227b11692d565
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-06-03 09:56:20 +0200

    added more todos

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -4,8 +4,32 @@ export default function TodoApp() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      title: "Springa 10 km",
+      title: "Springa 10km",
       description: "5:00 pace i skogsmiljö",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Springa 5km",
+      description: "4:00 pace i skogsmiljö",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "Springa 5km",
+      description: "3:00 pace på asfalt",
+      done: false,
+    },
+    {
+      id: 4,
+      title: "Springa 30km",
+      description: "5:30 pace i skogsmiljö",
+      done: false,
+    },
+    {
+      id: 5,
+      title: "Springa 1km",
+      description: "Det snabbaste du kan",
       done: false,
     },
   ]);
