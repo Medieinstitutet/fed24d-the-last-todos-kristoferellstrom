08509a37822b761c3fb67c005b3dea11c835a0a4
Kristofer <kristofer.sven.ellstrom@gmail.com>
2025-06-03 09:18:37 +0200

    feat: display todo anf useState

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 export default function TodoApp() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Springa 10 km", 
+        description: "5:00 pace i skogsmiljö", 
+        done: false 
+    }
+  ]);
+
   return (
     <div>
       <h1>Todo</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
